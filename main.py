adfc9e805ffa21284a4bc8944dfa70f8e9e55c66
kovaliovev <krazy.kavo@gmail.com>
2022-08-18 20:13:34 +0300

    bug fix

--- main.py
+++ main.py
@@ -9,30 +9,30 @@ while play.lower() == 's':
     count = 1
 
     while leng.lower() != 'uk' and leng.lower() != 'en':
-        leng = input('Please choose an available language')
+        leng = input('Please choose an available language \n')
 
     if leng.lower() == 'uk':
         print('Число загадане, спробуйте вiдгадати його якомога швидше!')
-        trying = int(input('Введiть ваше число вiд 1 до 100'))
+        trying = int(input('Введiть ваше число вiд 1 до 100 \n'))
         while trying != target:
             if trying > target:
-                trying = int(input('Занадто багато, спробуйте ще раз'))
+                trying = int(input('Занадто багато, спробуйте ще раз \n'))
                 count += 1
             else:
-                trying = int(input('Занадто мало, спробуйте ще раз'))
+                trying = int(input('Занадто мало, спробуйте ще раз \n'))
                 count += 1
         print(f'Вiтаю, ви вiдгадали! Кiлькiсть ваших спроб: {count}')
-        play = input('Введіть "S", щоб знову грати чи щось інше, щоб вийти')
+        play = input('Введ iть "S", щоб знову грати чи щось  iнше, щоб вийти \n')
 
     elif leng.lower() == 'en':
         print('The number is guessed, try to guess it as soon as possible!')
-        trying = int(input('Enter your number from 1 to 100'))
+        trying = int(input('Enter your number from 1 to 100 \n'))
         while trying != target:
             if trying > target:
-                trying = int(input('Too many, try again'))
+                trying = int(input('Too many, try again \n'))
                 count += 1
             else:
-                trying = int(input('Too little, try again'))
+                trying = int(input('Too little, try again \n'))
                 count += 1
         print(f'Congratulations, you guessed it! Number of your attempts: {count}')
-        play = input('Enter "S" to play again or something else to exit')
+        play = input('Enter "S" to play again or something else to exit \n')
